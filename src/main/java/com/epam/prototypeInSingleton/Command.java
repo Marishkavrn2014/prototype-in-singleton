f68612b7325769b32adf679757149bd5c294f88a
Marina Shadrina <Marina_Shadrina@epam.com>
2021-04-12 10:14:03 +0300

    initial commit

---
+++ src/main/java/com/epam/prototypeInSingleton/Command.java
@@ -0,0 +1,8 @@
+package com.epam.prototypeInSingleton;
+
+import org.springframework.shell.standard.ShellComponent;
+
+@ShellComponent
+public class Command {
+
+}
---
+++ src/main/java/com/epam/prototypeInSingleton/PrototypeInSingletonApplication.java
@@ -0,0 +1,13 @@
+package com.epam.prototypeInSingleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeInSingletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeInSingletonApplication.class, args);
+	}
+
+}
